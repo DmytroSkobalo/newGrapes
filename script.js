5c372da9fffc878c8cec3a5aa92431185b85d8d1
Dmytro Skobalo <skobalo88@mail.ru>
2016-08-07 12:10:17 +0000

    change commit

--- script.js
+++ script.js
@@ -24,17 +24,45 @@ $(document).ready(function() {
 
 $('.best_wine figure img');
 
- $('img[src*=cabernet2]').hide(0);
- $('img[src*=cabernet2]').show(3000);
- $('img[src*=chardone3]').hide(0);
- $('img[src*=chardone3]').show(3000);
- $('img[src*=kahor2]').hide(0);
- $('img[src*=kahor2]').show(3000);
- $('#img img[src*=kupaj]').hide(0);
- $('#img img[src*=kupaj]').show(3000);
- $('img[src*=merlot2]').hide(0);
- $('img[src*=merlot2]').show(3000);
- $('img[src*=muscat2]').hide(0);
- $('img[src*=muscat2]').show(3000);
+var myImg = $('img[src*=cabernet2]');
+   myImg.fadeOut(0).fadeIn(5000);
+var myImg2 = $('img[src*=chardone3]');
+   myImg2.hide(0).show(3000);
+var myImg3 = $('img[src*=kahor2]');
+   myImg3.hide(0).show(3000);
+var myImg4 = $('#img img[src*=kupaj]');
+   myImg4.hide(0).show(3000);
+ var myImg5 = $('img[src*=merlot2]');
+   myImg5.hide(0).show(3000);
+ var myImg6 = $('img[src*=muscat2]');
+   myImg6.hide(0).show(3000);
+ 
+ /*var heading = $('#heading '); 
+   heading.hide(0).show(2000);*/
+
+   $('#heading').slideUp(0).slideDown(3000);
     
+    
+ /*   function elementOut (element, myTime) {
+        
+        if (myTime > 10000 || myTime < 500 || isNaN(myTime)) {
+            return false
+        }  
+        else {
+            idElement = '#' + element;
+            $(idElement).fadeOut(myTime).fadeIn(myTime);
+        }    
+        
+        
+    
+    }
+    
+     elementOut('img', 5000); */
+     
+   var look =  $('#img [src*= index]');
+      look.fadeTo(7000, 0.1).fadeTo(5000, 1);
+      
+      $('#wrapper h1').slideUp(0).slideDown(3000);
 }); 
+
+
