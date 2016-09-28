2c84a8cb518980f3289089a4c52253c91d084f11
Dmytro Skobalo <skobalo88@mail.ru>
2016-09-28 22:29:52 +0000

    change commit

--- script.js
+++ script.js
@@ -108,3 +108,48 @@ $(" #nav li").hover(function(){
  $(document).ready(function(){
 	mainmenu();
 }); 
+
+
+
+
+
+
+(function() {
+  [].slice.call(
+    document.querySelectorAll(
+      'input.input__field'
+    )).forEach(function(inputEl) {
+      if( inputEl.value.trim() !== '' ) {
+              classie.add(
+          inputEl.parentNode,
+          'input--filled'
+        );
+      }
+
+            inputEl.addEventListener(
+        'focus',
+        onInputFocus
+      );
+            inputEl.addEventListener(
+        'blur',
+        onInputBlur
+      );
+    }
+  );
+
+    function onInputFocus(ev) {
+    classie.add(
+      ev.target.parentNode,
+      'input--filled'
+    );
+  }
+
+    function onInputBlur(ev) {
+    if(ev.target.value.trim() === '') {
+      classie.remove(
+        ev.target.parentNode,
+        'input--filled'
+      );
+    }
+  }
+})();
